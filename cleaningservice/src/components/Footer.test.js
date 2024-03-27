6a74e12f90a9f9b437986381634d4b41269b3a19
akshayaca <akshayaca@outlook.com>
2024-03-27 01:47:39 -0400

    added header message

--- cleaningservice/src/components/Footer.test.js
+++ cleaningservice/src/components/Footer.test.js
@@ -1,3 +1,13 @@
+/*
+  File: Footer.test.js
+  Description: Unit tests for ServiceListing component.
+  Author: Akshayarekha Subhramaniyan
+  Date: 27/03/2024
+
+  Note: This file contains unit tests for the ServiceListing component. It tests rendering of service listings
+  with different filter options.
+*/
+
 import React from 'react';
 import { render } from '@testing-library/react';
 import Footer from './Footer';
